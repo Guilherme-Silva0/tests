2b7da648f557c3f3aa37d3003bc1a9b440028b0e
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-09 22:46:34 -0300

    finalizando o frontend

--- get-ip/frontend/src/App.jsx
+++ get-ip/frontend/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [ip, setIp] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getIP = async () => {
+    setIsLoading(true);
+    const res = await axios.get("https://get-ip.guilhermesil189.repl.co");
+    setIsLoading(false);
+    setIp(res.data.ip);
+  };
   return (
     <div className="container">
-      <h1>Seu ip é: </h1>
-      <button>Qual meu ip?</button>
+      {ip ? <h1>Seu ip é: {ip}</h1> : ""}
+      <button onClick={() => getIP()}>
+        {isLoading ? "Carregando..." : "Qual meu ip?"}
+      </button>
     </div>
   );
 }
