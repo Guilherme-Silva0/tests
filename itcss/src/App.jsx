3062c3a1f024e07f3e5a402c8c8af21cfeec7f11
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-03-23 23:56:48 -0300

    teste de ITCSS

--- itcss/src/App.jsx
+++ itcss/src/App.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import Title from "./objects/Title";
+import Button from "./objects/Button";
+
 const App = () => {
-  return <h1>App</h1>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <Counter className="counter">
+      <Title>{count === 0 ? "Eita" : `Eita ${count}X`}</Title>
+      <Button className="button" onClick={() => setCount(count + 1)}>
+        Adcionar
+      </Button>
+    </Counter>
+  );
 };
 
 export default App;
