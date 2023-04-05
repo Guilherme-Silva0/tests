97d86077b394a9b5edb4704a521672bb9ad5bbf3
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-05 19:01:01 -0300

    configurando envio de email

--- SendEmailNode/backend/sendEmail.js
+++ SendEmailNode/backend/sendEmail.js
@@ -1,6 +1,31 @@
 const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-const sendEmail = () => {};
+const sendEmail = (to, subject, message) => {
+  const transport = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, //true para 465, false para as outras
+    auth: {
+      user: process.env.EMAIL_HOST,
+      pass: process.env.PASS_EMAIL_HOST,
+    },
+  });
+
+  transport
+    .sendMail({
+      from: "Guilherme Silva <" + process.env.EMAIL_HOST + ">",
+      to: to,
+      subject: subject,
+      html: message,
+      text: message,
+    })
+    .then(() => {
+      return true;
+    })
+    .catch(() => {
+      return false;
+    });
+};
 
 module.exports = sendEmail;
