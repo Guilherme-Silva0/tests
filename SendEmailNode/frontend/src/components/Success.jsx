a9f9c748d4b6bb2ef498446552823c3a65a0c385
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-09 17:17:13 -0300

    criando componente sucess

---
+++ SendEmailNode/frontend/src/components/Success.jsx
@@ -0,0 +1,11 @@
+const Success = () => {
+  return (
+    <div className="w-full px-5 py-3 bg-green-600 rounded-xl shadow-md transition-all">
+      <p className="w-full break-words text-center font-semibold text-gray-100">
+        E-mail enviado com sucesso!
+      </p>
+    </div>
+  );
+};
+
+export default Success;
