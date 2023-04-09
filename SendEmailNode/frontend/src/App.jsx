98610d600478dc923bfb3a1bb99f7b4c373687d2
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-09 16:01:41 -0300

    validando form

--- SendEmailNode/frontend/src/App.jsx
+++ SendEmailNode/frontend/src/App.jsx
@@ -1,8 +1,42 @@
 import "./global.css";
+import { useState } from "react";
+import * as Yup from "yup";
 import Form from "./components/Form";
+import { useApi } from "./hooks";
+
+const schema = Yup.object().shape({
+  fate: Yup.string()
+    .email("Digite um email válido")
+    .required("Todos os campos são obrigatórios")
+    .trim(),
+  subject: Yup.string().required("Todos os campos são obrigatórios").trim(),
+  message: Yup.string().required("Todos os campos são obrigatórios").trim(),
+});
 
 const App = () => {
-  return <Form />;
+  const [errors, setErrors] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = async (values) => {
+    try {
+      await schema.validate(values);
+      setErrors("");
+      const api = useApi();
+      const res = await api.send({
+        to: values.fate,
+        subject: values.subject,
+        message: values.message,
+      });
+      if (res) {
+        console.log(res);
+      } else {
+        console.log(res);
+      }
+    } catch (err) {
+      setErrors(err.errors);
+    }
+  };
+  return <Form handleSubmit={handleSubmit} errors={errors} isLoading />;
 };
 
 export default App;
