0e4cc9c897281c7b7c9412b9ff615143ffd44354
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-09 17:15:49 -0300

    estilizando botão desabilitado

--- SendEmailNode/frontend/src/components/Button.jsx
+++ SendEmailNode/frontend/src/components/Button.jsx
@@ -1,8 +1,9 @@
-const Button = ({ children, type }) => {
+const Button = ({ children, type, disabled }) => {
   return (
     <button
       type={type}
-      className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-1 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 shadow-md"
+      disabled={disabled}
+      className="bg-blue-600 hover:bg-blue-700 text-white font-medium py-1 px-6 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:ring-opacity-50 disabled:bg-slate-600 disabled:cursor-default shadow-md cursor-pointer"
     >
       {children}
     </button>
