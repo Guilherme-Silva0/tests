3a6f745fec65bba5b18d4fe3ff5cb75dad341d0a
Guilherme Silva <luiz.silva136@aluno.ce.gov.br>
2023-04-09 16:02:39 -0300

    enviando dados do formulario

--- SendEmailNode/frontend/src/components/Form.jsx
+++ SendEmailNode/frontend/src/components/Form.jsx
@@ -1,10 +1,23 @@
 import Input from "./Input";
 import TextArea from "./TextArea";
 import Button from "./Button";
+import Error from "./Error";
 
-const Form = () => {
+const Form = ({ handleSubmit, errors }) => {
   return (
-    <form className="flex flex-col items-center justify-center gap-3 bg-gray-800 p-6 rounded-xl shadow-lg mx-4">
+    <form
+      className="flex flex-col items-center justify-center gap-3 bg-gray-800 p-6 rounded-xl shadow-lg w-80 mx-4"
+      onSubmit={(e) => {
+        e.preventDefault();
+        const { fate, subject, message } = e.target.elements;
+        handleSubmit({
+          fate: fate.value,
+          subject: subject.value,
+          message: message.value,
+        });
+      }}
+    >
+      {errors && <Error>{errors}</Error>}
       <div className="flex flex-col gap-1 w-full">
         <label htmlFor="fate" className="text-gray-200 font-medium">
           Destinatario
