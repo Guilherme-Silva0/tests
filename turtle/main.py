c808a195c8c4d2d589bb7d76a3716daba667c292
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-28 19:51:08 -0300

    desenhando coração

--- turtle/main.py
+++ turtle/main.py
@@ -15,21 +15,39 @@ import turtle
 
 # SOL
 
-def draw_sun():
+# def draw_sun():
+#     t = turtle.Turtle()
+#     t.speed(2) 
+
+#     t.penup()
+#     t.goto(0, -100) 
+#     t.pendown()
+
+#     t.color("yellow")  
+
+#     for _ in range(36):
+#         t.forward(200) 
+#         t.right(170)  
+
+#     turtle.done()
+
+# draw_sun()
+
+# CORAÇÂO
+
+def draw_heart():
     t = turtle.Turtle()
-    t.speed(2) 
 
-    t.penup()
-    t.goto(0, -100) 
-    t.pendown()
-
-    t.color("yellow")  
-
-    for _ in range(36):
-        t.forward(200) 
-        t.right(170)  
+    t.fillcolor("red") 
+    t.begin_fill()
+    t.left(140)
+    t.forward(180)
+    t.circle(-90, 200)
+    t.left(120)
+    t.circle(-90, 200)
+    t.forward(180)
+    t.end_fill()
 
     turtle.done()
 
-draw_sun()
-
+draw_heart()
