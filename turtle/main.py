cf476e83a7594d1a84765f0ca7fafa2fc6c931d2
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-05-28 19:50:30 -0300

    desenhando sol

--- turtle/main.py
+++ turtle/main.py
@@ -2,13 +2,34 @@ import turtle
 
 # ESTRELA
 
-def draw_star():
-    t = turtle.Turtle()
+# def draw_star():
+#     t = turtle.Turtle()
 
-    for _ in range(5):
-        t.forward(100)
-        t.right(144)
+#     for _ in range(5):
+#         t.forward(100)
+#         t.right(144)
+
+#     turtle.done()
+
+# draw_star()
+
+# SOL
+
+def draw_sun():
+    t = turtle.Turtle()
+    t.speed(2) 
+
+    t.penup()
+    t.goto(0, -100) 
+    t.pendown()
+
+    t.color("yellow")  
+
+    for _ in range(36):
+        t.forward(200) 
+        t.right(170)  
 
     turtle.done()
 
-draw_star()
+draw_sun()
+
