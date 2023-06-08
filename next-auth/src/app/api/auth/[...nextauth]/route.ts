e5dc2ca3bb9efdee29c1895e4581f30cf4fb2063
Guilherme-silva0 <guilhermesilvadev0@gmail.com>
2023-06-08 11:09:44 -0300

    login

--- next-auth/src/app/api/auth/[...nextauth]/route.ts
+++ next-auth/src/app/api/auth/[...nextauth]/route.ts
@@ -5,6 +5,7 @@ import CredentialsProvider from 'next-auth/providers/credentials'
 import GoogleProvider from 'next-auth/providers/google'
 import GithubProvider from 'next-auth/providers/github'
 import { NextAuthOptions } from 'next-auth'
+import bcrypt from 'bcrypt'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -25,7 +26,28 @@ export const authOptions: NextAuthOptions = {
         username: { label: 'username', type: 'text' },
       },
       async authorize(credentials, req) {
-        const user = { id: '1', name: 'J Smith', email: 'jsmith@example.com' }
+        if (!credentials?.email?.trim() || !credentials?.password?.trim()) {
+          throw new Error('Invalid Credentials')
+        }
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials?.email,
+          },
+        })
+
+        if (!user || !user?.hashedPassword) {
+          throw new Error('Invalid Credentials')
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.hashedPassword,
+        )
+
+        if (!passwordMatch) {
+          throw new Error('Invalid Credentials')
+        }
 
         if (!user) {
           return null
